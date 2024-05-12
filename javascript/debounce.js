b21d1d0843c0ac1c0bc9d95ea247d47011b3b220
cliffordmirschel <cliff.mirschel@yahoo.com>
2024-05-12 10:22:38 -0400

    Added two solutions

---
+++ javascript/debounce.js
@@ -0,0 +1,11 @@
+var debounce = function (fn, t) {
+    let timer
+    return function (...args) {
+        clearTimeout(timer)
+        timer = setTimeout(() => fn(...args), t)
+    }
+};
+
+/**
+ * const log = debounce(console.log, 100);
+ * log('Hello'); // cancelled
---
+++ python/reshape_data.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def concatenateTables(df1: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
+    vertical_concat = pd.concat([df1, df2], axis=0)
+    return vertical_concat
+
+def alt_solution(df1: pd.DataFrame, df2: pd.DataFrame):
+    df3 = pd.concat([df1, df2])
+    return df3

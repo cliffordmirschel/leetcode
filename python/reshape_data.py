824f0031cc90b36d5bfc7bc1e4bd4f43a28ce564
cliffordmirschel <cliff.mirschel@yahoo.com>
2024-05-28 10:21:43 -0400

    Added pandas solutions

--- python/reshape_data.py
+++ python/reshape_data.py
@@ -7,3 +7,4 @@ def concatenateTables(df1: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
 def alt_solution(df1: pd.DataFrame, df2: pd.DataFrame):
     df3 = pd.concat([df1, df2])
     return df3
+

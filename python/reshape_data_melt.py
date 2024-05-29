85f3e79090dbf316b745e608d93f3b364463ea1f
cliffordmirschel <cliff.mirschel@yahoo.com>
2024-05-28 22:41:33 -0400

    added solutions

---
+++ python/reshape_data_melt.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+def meltTable(report: pd.DataFrame) -> pd.DataFrame:
+    report = report.melt(
+        id_vars=["product"],
+        value_vars=["quarter_1", "quarter_2", "quarter_3", "quarter_4"],
+        var_name="quarter",
+        value_name="sales",
+    )
+    return report
+# Optimized Solution
+def melt_table(report: pd.DataFrame) -> pd.DataFrame:
+    return pd.melt(report, id_vars='product', var_name='quarter', value_name='sales')
